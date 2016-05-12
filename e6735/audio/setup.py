8f324d0083f50c1b1af3c73d1d9c0b82e982d02a
voodooinnng <voodooinnng@gmail.com>
2016-05-11 21:52:09 -0400

    haha

--- e6735/audio/setup.py
+++ e6735/audio/setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
-
-import numpy
-from Cython.Build import cythonize
-from Cython.Distutils import Extension
+# from distutils.core import setup
+#
+# import numpy
+# from Cython.Build import cythonize
+# from Cython.Distutils import Extension
 
 # setup(ext_modules=[Extension('audio_feat', ['audio_feat.c'],
 #                              include_dirs=[numpy.get_include()],
